01d30046a52b59218854cd65656276fe999a3865
Manuel Yabes <imanuelyabesvernanda@gmail.com>
2022-11-09 07:47:43 +0700

    update procfile

--- ACIO/settings.py
+++ ACIO/settings.py
@@ -113,7 +113,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -125,11 +124,9 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR.joinpath('staticroot')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
