e2420b7a86f5904d3480e2a717ff03dece79f0be
Manuel Yabes <imanuelyabesvernanda@gmail.com>
2022-11-09 10:08:34 +0700

    update style and adduser

--- APP/urls.py
+++ APP/urls.py
@@ -4,9 +4,8 @@ from APP import views
 app_name = 'APP'
 
 urlpatterns = [
-    path('',views.base,name='base'),
-    path('index/',views.index,name='index'),
-    path('signup/',views.signup,name='signup'),
+    path('',views.index,name='index'),
+    path('adduser/',views.signup,name='signup'),
     path('register/',views.register,name='register'),
     path('login/',views.user_login,name='login'),
     path('logout/',views.user_logout,name='logout'),
